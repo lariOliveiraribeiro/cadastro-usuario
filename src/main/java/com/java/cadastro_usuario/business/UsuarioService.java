806da98eeb584a3750b09fb6646b8a7a78df75fd
Larissa Oliveira <166954911+lariOliveiraribeiro@users.noreply.github.com>
2026-01-17 18:17:49 -0300

    Initial commit

---
+++ src/main/java/com/java/cadastro_usuario/business/UsuarioService.java
@@ -0,0 +1,42 @@
+package com.java.cadastro_usuario.business;
+
+import com.java.cadastro_usuario.infrastructere.entitys.Usuario;
+import com.java.cadastro_usuario.infrastructere.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository repository;
+
+    public UsuarioService(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    public void salvarUsuario(Usuario usuario){
+        repository.saveAndFlush(usuario);
+    }
+
+    public Usuario buscarUsuarioPorEmail(String email) {
+
+        return repository.findByEmail(email).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado")
+        );
+    }
+
+    public void deletarUsuarioPorEmail(String email){
+        repository.deleteByEmail(email);
+    }
+
+    public void atualizarUsuarioPorId(Integer id, Usuario usuario){
+        Usuario usuarioEntity = repository.findById(id).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+        Usuario usuarioAtualizado = Usuario.builder()
+                .email(usuario.getEmail() != null ? usuario.getEmail() : usuarioEntity.getEmail())
+                .nome(usuario.getNome() != null ? usuario.getNome() : usuarioEntity.getNome())
+                .id(usuarioEntity.getId())
+                .build();
+        repository.saveAndFlush(usuarioAtualizado);
+
+    }
+}
+
---
+++ src/main/java/com/java/cadastro_usuario/infrastructere/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.java.cadastro_usuario.infrastructere.repository;
+
+import com.java.cadastro_usuario.infrastructere.entitys.Usuario;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository <Usuario, Integer> {
+
+    Optional <Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+}
